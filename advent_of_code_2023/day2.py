3251c3ca475aa447163bf037f870647d5f6662a5
João Gomes <j.gomes@runtime-revolution.com>
2023-12-03 23:17:08 +0000

    wip

---
+++ advent_of_code_2023/day2.py
@@ -0,0 +1,67 @@
+import pathlib
+import sys
+
+
+def load_input(data_path):
+    data = []
+    with open(data_path, "r") as reader:
+        data = reader.readlines()
+    return data
+
+
+def game_number(game):
+    return game.split(":")[0].split(" ")[1]
+
+
+def game_check(game):
+    cube_set = game.split(":")[1].split(";")
+
+    red_amount = 0
+    green_amount = 0
+    blue_amount = 0
+
+    for entry in cube_set:
+        draft = entry.split(", ")
+        for cube in draft:
+            cube_detail = cube.split(" ")
+            color = cube_detail[1]
+            amount = cube_detail[0]
+
+            if color == "red":
+                red_amount += int(amount)
+            elif color == "green":
+                green_amount += int(amount)
+            elif color == "blue":
+                blue_amount += int(amount)
+
+    if red_amount > 12 or green_amount > 13 or blue_amount > 14:
+        return False
+    return True
+
+
+def core(data):
+    possible_games = []
+    for game in data:
+        number = game_number(game)
+        if game_check(game):
+            possible_games.append(int(number))
+    return possible_games
+
+
+def save_output(data):
+    with open("output/day_two.txt", "w") as writer:
+        for item in data:
+            writer.write(f"{item}\n")
+
+
+def calculate_output(data):
+    return sum(data)
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        data = load_input(path)
+        ouput_data = core(data)
+        save_output(ouput_data)
+        final_sum = calculate_output(ouput_data)
+        print(final_sum)
