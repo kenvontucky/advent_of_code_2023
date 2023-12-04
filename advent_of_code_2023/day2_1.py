3b0eeeb4d179abf9995d6715e358306aa2874e59
João Gomes <j.gomes@runtime-revolution.com>
2023-12-04 00:06:06 +0000

    Day 2: Cube Conundrum (#2) part 1

---
+++ advent_of_code_2023/day2_1.py
@@ -0,0 +1,58 @@
+import pathlib
+import sys
+
+
+def load_input(data_path):
+    data = []
+    with open(data_path, "r") as reader:
+        data = reader.readlines()
+    return data
+
+
+def game_number(game):
+    return game.split(":")[0].split(" ")[1]
+
+
+def game_check(game):
+    cube_set = game.split(":")[1].split(";")
+
+    for entry in cube_set:
+        draft = entry.split(", ")
+        for cube in draft:
+            amount, color = cube.split()
+
+            if color == "red" and int(amount) > 12:
+                return False
+            elif color == "green" and int(amount) > 13:
+                return False
+            elif color == "blue" and int(amount) > 14:
+                return False
+    return True
+
+
+def core(data):
+    possible_games = []
+    for game in data:
+        number = game_number(game)
+        if game_check(game):
+            possible_games.append(int(number))
+    return possible_games
+
+
+def save_output(data):
+    with open("output/day_two.txt", "w") as writer:
+        for item in data:
+            writer.write(f"{item}\n")
+
+
+def calculate_output(data):
+    return sum(data)
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        data = load_input(path)
+        ouput_data = core(data)
+        save_output(ouput_data)
+        final_sum = calculate_output(ouput_data)
+        print(final_sum)
---
+++ tests/test_day2.py
@@ -0,0 +1,21 @@
+from advent_of_code_2023.day2 import core, calculate_output
+
+data = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
+
+def test_core():
+    possible_games = core(data)
+
+    assert sum(possible_games) == 8
+
+
+def test_calculate_output():
+    data = [1, 2, 3]
+
+    assert calculate_output(data) == 6
